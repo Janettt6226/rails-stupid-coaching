43fab40d582ca60ed38cdfcbbef7f376dd401697
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-08-14 14:36:25 +0200

    stupid coach completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get '/ask', to: 'questions_controller#ask'
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'questions#home'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
